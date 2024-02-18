bfc1b776fd28457f5b22a14fcb3e0d7aeb03b46d
amypo-reactapp <reactapp@amypo.in>
2024-02-18 04:29:22 +0000

    User

---
+++ src/Functioncomponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Functioncomponent = () =>{
+    return(
+        <div>
+            <h1>Hello Tharani!</h1>
+            </div>
+    )
+}
+export default Functioncomponent;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Functioncomponent from './Functioncomponent';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Functioncomponent/>
     
   </React.StrictMode>
 );
